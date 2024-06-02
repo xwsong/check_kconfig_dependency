f2b181e63bfdfd88dbcca482d1889524b5afcc9f
Xiongwei Song <xiongwei.song@windriver.com>
2024-06-02 22:51:58 +0800

    ckd.sh: check another corner case
    For example:
        i915-$(CONFIG_HWMON) += \
               i915_hwmon.o
    
    Signed-off-by: Xiongwei Song <xiongwei.song@windriver.com>

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./ckd.sh -f /nobackup/xsong2/workspace/linux/drivers/gpu/drm/i915/i915_gem_evict.c -s /nobackup/xsong2/workspace/linux
+./ckd.sh -f /nobackup/xsong2/workspace/linux/drivers/gpu/drm/i915/i915_hwmon.c -s /nobackup/xsong2/workspace/linux
 ./ckd.sh -f /nobackup/xsong2/workspace/linux/drivers/edac/zynqmp_edac.c -s /nobackup/xsong2/workspace/linux
 ./ckd.sh -f /nobackup/xsong2/workspace/linux/drivers/usb/core/port.c -s /nobackup/xsong2/workspace/linux
